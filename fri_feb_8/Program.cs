b451da757153df4287a6feb4cf86820d7f67280d
visualstudio <visualstudio@122-PC27>
2019-02-08 18:09:16 -0500

    6pm commit

--- fri_feb_8/Program.cs
+++ fri_feb_8/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace fri_feb_8
+namespace FRI_FEB_8
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Elevator
+    {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void run()
+        {
+            FirstFloor = new Node();
+            FirstFloor.FloorNumber = "First Floor";
+            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+
+            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
         }
     }
 }
